43b40b022d0d26b3a269dda2704e8ee3fe1faf72
kianfikram <fikramayyubi@gmail.com>
2024-10-29 09:21:09 +0700

    .

--- 1UI_UX.js
+++ 1UI_UX.js
@@ -5,12 +5,14 @@ function updateCountdown() {
   const timeLeft = countdownDate - now;
 
   if (timeLeft > 0) {
+    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
     const hours = Math.floor(
       (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
     );
     const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
+    document.getElementById("days").textContent = days;
     document.getElementById("hours").innerText = String(hours).padStart(2, "0");
     document.getElementById("minutes").innerText = String(minutes).padStart(
       2,
